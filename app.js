559ce34e5d1fd75892b673cb7ddcb3974587ebac
Shanti-kumari9 <shantik20@navgurukul.org>
2021-12-02 22:10:30 +0530

    frist comit

---
+++ app.js
@@ -0,0 +1,35 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+
+function errorHandler(error) {
+    console.log("error occured" ,error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+     var inputText = txtInput.value;     //taking input
+
+    //colling server for processing...
+    fetch(getTranslationURL(inputText))
+        .then (Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    
+};
+
+btnTranslate.addEventListener("click",clickHandler)
+
+
